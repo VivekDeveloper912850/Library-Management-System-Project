cc02559943375a2736137d8f9f2c99528e54621a
VivekDeveloper912850 <139674989+VivekDeveloper912850@users.noreply.github.com>
2025-04-14 11:00:07 +0530

    Update Login.jsx

--- Library-Frontend/src/pages/Login.jsx
+++ Library-Frontend/src/pages/Login.jsx
@@ -89,7 +89,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8080/api/login", credentials);
+      const response = await axios.post("https://lms-backend-production-0f0a.up.railway.app/api/login", credentials);
       alert("Login successful!");
       localStorage.setItem("userToken", response.data.token);
       localStorage.setItem("userId", response.data);
